a1c1dad5756e854c030281683cad1d01e7678399
ninamit <133363888+ninamit@users.noreply.github.com>
2023-05-21 23:34:54 +0200

    Update ConsoleGames1.sql

--- ConsoleGames1.sql
+++ ConsoleGames1.sql
@@ -18,3 +18,13 @@ SELECT COUNT(platform_name)  --Platforms released before Black Friday and Christ
 FROM console_dates
 WHERE DATE_PART('month',first_retail_availability) - 12= 0 OR DATE_PART('month', first_retail_availability)-11=0
 
+SELECT to_date(CAST(game_year AS varchar(4)), 'yyyy') --Convert to different data type, 5th assignment
+FROM public.console_GAMES
+ORDER BY game_rank
+
+SELECT *     --How to deal with missing data, 6th assignment
+FROM public.console_games
+WHERE jp_sales IS NULL
+UPDATE public.console_games
+SET jp_sales= round((na_sales+ eu_sales+ other_sales)/3)
+WHERE game_name= 'Brain Age: Train Your Brain in Minutes a Day'
