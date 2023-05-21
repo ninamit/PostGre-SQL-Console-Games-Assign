aaa3a208d13670f978b1539ea4dfe21595a493c1
ninamit <133363888+ninamit@users.noreply.github.com>
2023-05-21 22:28:39 +0200

    Update ConsoleGames1.sql

--- ConsoleGames1.sql
+++ ConsoleGames1.sql
@@ -11,7 +11,10 @@ UPDATE console_games
 SET na_sales_perc = na_sales / global_sales * 100
 WHERE global_sales > 0
 
-SELECT publisher, LEFT(publisher, 4) --First 4 letters of publisher name, 3rd assignement
+SELECT publisher, LEFT(publisher, 4) --First 4 letters of publisher name, 3rd assignment
 FROM public.console_games
 
+SELECT COUNT(platform_name)  --Platforms released before Black Friday and Christmas, 4th assignment
+FROM console_dates
+WHERE DATE_PART('month',first_retail_availability) - 12= 0 OR DATE_PART('month', first_retail_availability)-11=0
 
